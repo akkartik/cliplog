8e2f09b036204027037fa9432f91705a7587fcba
Kartik K. Agaram <vc@akkartik.com>
2014-08-19 17:36:36 -0700

    70

--- cliplog.c
+++ cliplog.c
@@ -7,8 +7,6 @@
 
 GtkClipboard* clipboard;
 
-GMutex* state_lock = NULL;
-
 /* Pass in the text via the struct. We assume len is correct, and BYTE based,
  * not character. Returns length of resulting string. */
 glong validate_utf8_text(gchar *text, glong len)
@@ -24,69 +22,28 @@ glong validate_utf8_text(gchar *text, glong len)
   return len;
 }
 
-struct history_item {
-	guint32 len; /**length of data item, MUST be first in structure  */
-	gchar text[8]; /**reserve 64 bits (8 bytes) for pointer to data.  */
-}__attribute__((__packed__));
-
-GList* history_list = NULL;
-
-void log_clipboard() {
+void log_clipboard(char* data) {
   static gchar* history_path = NULL;
   if (!history_path)
       history_path = g_build_filename(g_get_user_data_dir(), "clipboard_log", NULL);
   FILE* history_file = fopen(history_path, "a");
   if (!history_file) return;
-  /* Write most recent element */
   time_t now;
   time(&now);
-  fprintf(history_file, "-- %s%s\n", ctime(&now),
-      ((struct history_item*)history_list->data)->text);
+  fprintf(history_file, "-- %s%s\n", ctime(&now), data);
   fclose(history_file);
-}
-
-struct history_item *new_clip_item(guint32 len, void *data)
-{
-  struct history_item *c;
-  if (!(c=g_malloc0(sizeof(struct history_item)+len))) {
-    printf("Hit NULL for malloc of history_item!\n");
-    return NULL;
-  }
-
-  memcpy(c->text, data, len);
-  c->len = len;
-  return c;
-}
-
-void append_item(gchar* item)
-{
-  gint node = -1;
-  if (!item)
-    return;
-  g_mutex_lock(state_lock);
-
-  struct history_item *c;
-  if (!(c=new_clip_item(strlen(item), item)))
-    return;
-
-  history_list = g_list_prepend(history_list, c);
-  log_clipboard();
-  g_mutex_unlock(state_lock);
 }
 
 gchar* update_clipboard()
 {
-  /**current/last item in clipboard  */
   static gchar *curr = NULL;
-  gchar *cnext = NULL;
-
-  cnext = gtk_clipboard_wait_for_text(clipboard);
+  gchar *cnext = gtk_clipboard_wait_for_text(clipboard);
   if (cnext && g_strcmp0(curr, cnext)) {
     if (validate_utf8_text(cnext, strlen(cnext))) {
       if (curr)
         g_free(curr);
       curr = g_strdup(cnext);
-      if (curr) append_item(curr);
+      if (curr) log_clipboard(curr);
     }
   }
   if (cnext) g_free(cnext);
@@ -104,8 +61,6 @@ int main(int argc, char *argv[])
 
   clipboard = gtk_clipboard_get(GDK_SELECTION_CLIPBOARD);
   g_timeout_add(500/*ms*/, check_clipboards, NULL);
-
-  state_lock = g_mutex_new();
   gtk_main();
   return 0;
 }
